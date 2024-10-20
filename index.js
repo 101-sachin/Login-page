335711a0b1ad22f0006ac4ce148c6838248ffcf0
Sachin Yadav <yadavsachin6987@gmail.com>
2024-10-21 02:12:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let signinbtn = document.getElementById('signinbtn')
+let signupbtn = document.getElementById('signupbtn')
+let namefield = document.getElementById('name')
+let title = document.getElementById('title')
+
+signinbtn.onclick=function(){
+    
+}
